4f42443cc24d0c9b7ce8a95610243ea385e02f03
Chris Combs <chco@nodes.dk>
2016-07-01 15:17:17 +0200

    Initial project commit

---
+++ QRIO/QRIO.h
@@ -0,0 +1,19 @@
+//
+//  QRIO.h
+//  QRIO
+//
+//  Created by Chris on 01/07/16.
+//  Copyright © 2016 Nodes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QRIO.
+FOUNDATION_EXPORT double QRIOVersionNumber;
+
+//! Project version string for QRIO.
+FOUNDATION_EXPORT const unsigned char QRIOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QRIO/PublicHeader.h>
+
+
